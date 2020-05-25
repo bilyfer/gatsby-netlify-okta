b4304cf6ba479fb54b6ff6e3ccdcb0ce229f356a
bchavez <bchavez@aclaratech.com>
2020-05-24 18:29:59 -0600

    changin logo header to use Link intead of a

--- src/components/Header/logo.js
+++ src/components/Header/logo.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 function Logo() {
   return (
     <div id="masthead" className="bg-method logo-container">
-      <a href="/">
+      <Link to="/">
         <div id="logo"></div>
-      </a>
+      </Link>
     </div>
   )
 }
